cf0a8c12f65df1464abb31a0704335c2d9db6167
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2021-06-28 20:55:34 +0200

    exposed IotaDID and IotaDocument

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod identity_manager;
 pub use identity_manager::{IdentityManager, Validator};
 pub use identity::core::json;
+pub use identity::iota::{IotaDID, IotaDocument};
 pub use anyhow::{Result, Error};
